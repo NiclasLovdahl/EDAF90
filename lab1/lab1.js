51b4296c0d92803b9ab86f535c690b22e134bb73
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-01-29 18:41:26 +0100

    Salad class with functions add/remove/price implemented. Assignement 5-7 done.

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -31,3 +31,70 @@ console.log(
         "\n" +
         result.dressing
 );
+
+// Assingment 5-7
+
+class Salad {
+    constructor() {
+        this.foundation = [];
+        this.proteins = [];
+        this.extras = [];
+        this.dressing = [];
+    }
+
+    add(type, selection) {
+        switch (type) {
+            case "foundation":
+                this.foundation.push(selection);
+                break;
+            case "proteins":
+                this.proteins.push(selection);
+                break;
+            case "extras":
+                this.extras.push(selection);
+                break;
+            case "dressing":
+                this.dressing.push(selection);
+                break;
+            default:
+                console.error("Type not found.");
+                break;
+        }
+    }
+
+    remove(type, selection) {
+        switch (type) {
+            case "foundation":
+                this.foundation.splice(foundation.indexOf(selection, 1));
+                break;
+            case "proteins":
+                this.proteins.splice(proteins.indexOf(selection, 1));
+                break;
+            case "extras":
+                this.extras.splice(extras.indexOf(selection, 1));
+                break;
+            case "dressing":
+                this.dressing.splice(dressing.indexOf(selection, 1));
+                break;
+            default:
+                console.error("Type not found.");
+                break;
+        }
+    }
+
+    price() {
+        return this.foundation
+            .concat(this.proteins, this.extras, this.dressing)
+            .reduce((acc, curr) => {
+                return (acc += imported.inventory[curr].price);
+            }, 0);
+    }
+}
+
+let mySalad = new Salad();
+mySalad.add("foundation", "Salad + Glasnudlar");
+mySalad.add("proteins", "Handskalade räkor från Smögen");
+mySalad.add("extras", "Krossade jordnötter");
+mySalad.add("extras", "Parmesan");
+mySalad.add("dressing", "Kimchimayo");
+console.log(mySalad.price());
