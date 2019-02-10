2092df8a81af77ecfbc20362bb1e0ea0f801f557
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-02-10 20:05:30 +0100

    routing compose-salad and view-order added and working

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import shortid from "shortid";
 
 import inventory from "./lib/inventory.ES6";
-import ComposeSaladModal from "./components/ComposeSaladModal";
+// import ComposeSaladModal from "./components/ComposeSaladModal";
+import ComposeSalad from "./components/ComposeSalad";
 import ViewOrder from "./components/ViewOrder";
 
 class App extends Component {
@@ -15,6 +17,17 @@ class App extends Component {
         this.state = {
             salads: []
         };
+
+        this.composeSaladElem = params => (
+            <ComposeSalad
+                {...params}
+                inventory={inventory}
+                addSalad={this.addSalad}
+            />
+        );
+        this.viewOrderElem = params => (
+            <ViewOrder {...params} order={this.state.salads} />
+        );
     }
 
     addSalad = obj => {
@@ -32,12 +45,24 @@ class App extends Component {
                     <p className="lead">Lab 2</p>
                 </div>
                 <div className="App">
-                    <ComposeSaladModal
-                        inventory={inventory}
-                        addSalad={this.addSalad}
-                    />
+                    <ul className="nav nav-pills">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="compose-salad">
+                                Komponera din egen sallad
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="view-order">
+                                Order
+                            </Link>
+                        </li>
+                    </ul>
 
-                    <ViewOrder order={this.state.salads} />
+                    <Route
+                        path="/compose-salad"
+                        render={this.composeSaladElem}
+                    />
+                    <Route path="/view-order" render={this.viewOrderElem} />
                 </div>
             </>
         );
