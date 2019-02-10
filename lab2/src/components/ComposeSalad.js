6e6e73966aae46f54906389db478d11157a61818
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-02-10 07:30:09 +0100

    state add and remove working for all ingredients

--- lab2/src/components/ComposeSalad.js
+++ lab2/src/components/ComposeSalad.js
@@ -1,24 +1,111 @@
 import React, { Component } from "react";
 
+import SaladCheckbox from "./SaladCheckbox";
+import SaladSelect from "./SaladSelect";
+
 class ComposeSalad extends Component {
-    render() {
-        const inventory = this.props.inventory;
+    constructor(props) {
+        super();
+
+        this.state = {
+            foundation: "Sallad",
+            proteins: [],
+            extras: [],
+            dressing: "Ceasardressing"
+        };
+
+        const inventory = props.inventory;
+
         // test for correct ussage, the parent must send this datastructure
         if (!inventory) {
             alert("inventory is undefined in ComposeSalad");
         }
-        let foundations = Object.keys(inventory).filter(
+
+        this.foundations = Object.keys(inventory).filter(
             name => inventory[name].foundation
         );
+        this.proteins = Object.keys(inventory).filter(
+            name => inventory[name].protein
+        );
+        this.extras = Object.keys(inventory).filter(
+            name => inventory[name].extra
+        );
+        this.dressings = Object.keys(inventory).filter(
+            name => inventory[name].dressing
+        );
+    }
+
+    handleSelectChange = event => {
+        const name = event.target.getAttribute("name");
+        const value = event.target.value;
+
+        if (name === "foundation") {
+            this.setState({ foundation: value });
+        } else if (name === "dressing") {
+            this.setState({ dressing: value });
+        }
+    };
+
+    handleCheckboxChange = event => {
+        const name = event.target.getAttribute("name");
+        const value = event.target.value;
+
+        if (name === "protein") {
+            if (event.target.checked) {
+                this.setState({
+                    proteins: [...this.state.proteins, value]
+                });
+            } else {
+                var index = this.state.proteins.indexOf(value);
+                this.setState(this.state.proteins.splice(index, 1));
+            }
+        } else if (name === "extra") {
+            if (event.target.checked) {
+                this.setState({
+                    extras: [...this.state.extras, value]
+                });
+            } else {
+                var index = this.state.extras.indexOf(value);
+                this.setState(this.state.extras.splice(index, 1));
+            }
+        }
+    };
+
+    render() {
         return (
-            <div className="container">
-                <h4>Välj bas</h4>
-                <ul>
-                    {foundations.map(name => (
-                        <li key={name}>{name}</li>
-                    ))}
-                </ul>
-            </div>
+            <form>
+                <h4>Välj bas:</h4>
+                <SaladSelect
+                    type="foundation"
+                    items={this.foundations}
+                    init={this.state.foundation}
+                    handleChange={this.handleSelectChange}
+                />
+
+                <h4>Välj protein:</h4>
+                <SaladCheckbox
+                    type="protein"
+                    items={this.proteins}
+                    handleChange={this.handleCheckboxChange}
+                    itemList={this.state.proteins}
+                />
+
+                <h4>Välj extras:</h4>
+                <SaladCheckbox
+                    type="extra"
+                    items={this.extras}
+                    handleChange={this.handleCheckboxChange}
+                    itemList={this.state.extras}
+                />
+
+                <h4>Välj dressing</h4>
+                <SaladSelect
+                    type="dressing"
+                    items={this.dressings}
+                    init={this.state.dressing}
+                    handleChange={this.handleSelectChange}
+                />
+            </form>
         );
     }
 }
