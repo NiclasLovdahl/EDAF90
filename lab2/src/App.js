b116dbea900d85cf6c905d353425d86b10b32680
Niclas Lövdahl <niclas.lovdahl@gmail.com>
2019-02-10 07:26:07 +0100

    stripped away unwanted elements

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -2,27 +2,29 @@ import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import $ from "jquery"; // skip this if you do not use bootstrap modals
-import Popper from "popper.js"; // skip this if you do not use bootstrap modals
 
 import inventory from "./lib/inventory.ES6";
 import ComposeSaladModal from "./components/ComposeSaladModal";
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            salads: []
+        };
+    }
+
     render() {
         return (
-            <div className="App">
+            <>
                 <div className="jumbotron text-center">
                     <h1 className="display-4">EDAF90 - Web Programming</h1>
-                    <p className="lead">
-                        This is a template project for react + bootstrap.
-                    </p>
-                    <hr className="my-4" />
-                    <p>This code is a good starting point for lab 2.</p>
+                    <p className="lead">Lab 2</p>
                 </div>
 
                 <ComposeSaladModal inventory={inventory} />
-            </div>
+            </>
         );
     }
 }
